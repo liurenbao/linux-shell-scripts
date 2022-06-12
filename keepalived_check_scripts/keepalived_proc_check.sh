5ecdb3d0f0d4b8be021d8f9b0e9e1add2953e43a
noise131 <ayur1031@163.com>
2022-06-12 18:06:27 +0800

    v1

--- keepalived_check_scripts/keepalived_proc_check.sh
+++ keepalived_check_scripts/keepalived_proc_check.sh
@@ -4,7 +4,7 @@ pid_count=$(ps -aux |grep -w "${1}" |grep -vEc "${0}|grep")
 if [ "${pid_count}" != '0' ]; then
     exit 0
 fi
-port_count=$(ss -tlnp |grep -wc 'haproxy')
+port_count=$(ss -tlnp |grep -wc "${0}")
 if [ "${port_count}" != '0' ]; then
     exit 0
 fi
